6ed8a33ec6c2026bb2257db86cc24c73cb7b07ea
Oghenemaro <yourdurhamemail@example.com>
2021-12-15 01:52:19 -0500

    First commit

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'app_server', 'views')));
 
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
